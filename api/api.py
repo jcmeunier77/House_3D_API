13ed685b64f9cc8a101b45b1034c2076cd16b7ee
jcmeunier77 <jc.meunier77@gmail.com>
2020-11-02 08:25:09 +0100

    Finalized project uploaded

--- api/api.py
+++ api/api.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template, url_for, request, redirect, send_file
 from io import BytesIO
 from PIL import Image
 from flask_restx import Resource, Api
+from api.src.utils.a_addres_to_crs import AddressToCrs
+
+
+import folium
 from werkzeug.middleware.proxy_fix import ProxyFix
+from api.src import Construct3D
 
 # from api.utils import
 
@@ -12,19 +17,57 @@ app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 # api = Api(app)
 
 # creating route for typing address
-@app.route("/", methods=["POST", "GET"])
+@app.route("/", methods=["GET"])
 def adress_form():
-   if request.method == "POST":
-      street = request.form["street"]
-      return redirect(url_for("street", strt=street))
-   else :
-      return render_template("index.html")
+    return 'lapin'
 
-@app.route("/<strt>")
-def street(strt):
-   return f"<1>{strt}</h1>"
+#     pass
+#    if request.method == "POST":
+#       street = request.form["street"]
+#       return redirect(url_for("street", strt=street))
+#    else :
+#       return render_template("index.html")
+
+@app.route("/3d/<street>/<city>/<en3D>")
+def view_3d(street, city, en3D):
+    if en3D == "submit":
+        return Construct3D(street, city).constructor()
+        # return {
+        #         street: 'True',
+        #         city: 'The v2 API server is alive.',
+        #         en3D:'connerie'
+        #     }, 200
+
+@app.route("/map/<street1>/<city1>/<enMap>")
+def view_map(street1, city1, enMap):
+    if enMap == "encode":
+        Construct3D(street1, city1).map()
+        return render_template('map.html')
+        # print(TargetToMap(51.319986, 5.077554).to_map())
+        #
+        # targetLL = AddressToCrs(street1, city1).to_long_latt()
+        # mappy = folium.Map(location=[targetLL[0], targetLL[1]], zoom_start=17)
+        # folium.CircleMarker(location=[targetLL[0], targetLL[1]], radius=30, popup='Your address', color='#3186cc',
+        #                     fill=True, fill_color='#3186cc').add_to(mappy)
+        # mappy.save('map.html')
+        # return render_template()
+
+
+        #Construct3D(street1, city1).map()
+        #return render_template("map.html")
+
+    #print ({street:"rue", city:"rrr"})
+    # print(street, city, en3D)
+    # if en3D.value == 'submit':
+    #return Construct3D(street, city).constructor()
+         # return {
+         #         street1: 'True',
+         #         city1: 'The v2 API server is alive.',
+         #         enMap:'connerie'
+         #     }, 200
+
 
 # creating route for showing 3d building
-@app.route("/map3d")
-def map3d():
-   return render_template("map3d.html")
+# @app.route("/map3d")
+# def map3d():
+#    return render_template("map3d.html")
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # Imports
 import os
 from api import app_3D
+from api.src.utils.d_target_to_map import TargetToMap
 
 # Create the port
-port = int(os.environ.get("PORT", 5000))
+port = int(os.environ.get("PORT", 5002))
 
 
 # Run the app
 if __name__ == '__main__':
+    # print(TargetToMap(51.319986, 5.077554).to_map())
     app_3D.run('127.0.0.1', port=port, debug=False, threaded=False)
 
